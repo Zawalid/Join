8eebb1cabfd05c83b146c5db29141b13ad4602c2
Zawalid <walid.zakan@gmail.com>
2024-09-05 18:39:31 +0100

    set up the server structure

---
+++ server/controllers/authController.js
@@ -0,0 +1,5 @@
+const login = async (req, res) => {};
+
+const register = async (req, res) => {};
+
+module.exports = { login, register };
---
+++ server/index.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const express = require('express');
+// const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const cors = require('cors');
+const morgan = require('morgan');
+// const hpp = require('hpp');
+
+// Start express app
+const app = express();
+
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: 'Too many requests from this IP, please try again in an hour!'
+// });
+// app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+// app.use(
+//   hpp({
+//     whitelist: [
+
+//     ]
+//   })
+// );
+
+app.use(compression());
+
+// 3) ROUTES
+
+module.exports = app;
